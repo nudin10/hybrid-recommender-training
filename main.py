c6d77274cca93a2ce448024350e96098b2d9e735
nudin <ahmadazhar10@gmail.com>
2025-05-17 14:28:17 +0800

    implement try except

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ from src.tools.logger import init_global_logger
 from data_loader.data_loader import load_data_into_redis
 
 async def main():
-    r = init_redis()
-    init_telegram_bot()
-    init_global_logger()
+    try:
+        r = init_redis()
+        init_telegram_bot()
+        init_global_logger()
+    except Exception as e:
+        raise RuntimeError(f"Error setting up dependencies: {e}")
 
-    DATA_PATH="data"
-    await load_data_into_redis(r, DATA_PATH)
+    try:
+        DATA_PATH="data"
+        await load_data_into_redis(r, DATA_PATH)
+    except Exception as e:
+        raise RuntimeError(f"Error loading data into Redis: {e}")
 
 if __name__ == "__main__":
-    run(main())
+    try:
+        run(main())
+    except Exception as e:
+        print(f"Application terminated due to unhandled error: {e}")
+        exit(1)
